fd449b162daad0195875ab33783451fc25b4a287
glaindamping <s11810021@universitasklabat.ac.id>
2020-09-29 14:17:33 +0800

    eight commit

--- pertemuan11/index.js
+++ pertemuan11/index.js
@@ -65,13 +65,13 @@ console.log(calcAge(1991, 2021));
 
 //Exercise
 //Convert to ES6 Syntax
-const yearUntilRetirement = (year, firstName) => {
+const yearUntilRetirement = (year = 2020, firstName = "Glain") => {
   let age = 28;
   let retirement = 65 - age;
-  if (retirement > 0) {
-      console.log(firstName + " retired in " + retirement + " years ");
+  if(retirement > 0){
+      console.log(`${firstName} retired in ${retirement} years.`);
   } else {
-      console.log(firstName + " is already retired.");
+      console.log(`${firstName} is already retired.`);
   }
-  yearUntilRetirement(2001, Glain);
 }
+yearUntilRetirement();
