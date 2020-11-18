cbed6f4db0852e194dc82970a660191871d11e3f
glaindamping <glainharddamping@gmail.com>
2020-11-19 00:48:14 +0800

    gabungan pertemuan 21-24

--- pertemuan20/src/components/card.js
+++ pertemuan20/src/components/card.js
@@ -1,12 +1,13 @@
   
-import react from 'react'
+import React from 'react'
 
-const Card = ({name, institution, address, phone}) =>{
+
+const Card = ({name, username, email, phone}) =>{
     return (
         <div className="card-container">
             <p>{name}</p>
-            <p>{institution}</p>
-            <p>{address}</p>
+            <p>{username}</p>
+            <p>{email}</p>
             <p>{phone}</p>
         </div>
     )
